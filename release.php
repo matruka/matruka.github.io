390903c4fdcae0e73fd03cbedb0ad17eebd78283
matruka <maxkulagin@yahoo.de>
2020-01-11 21:47:49 +0100

    .

--- release.php
+++ release.php
@@ -2,5 +2,5 @@
         include_once 'checker.php';
         
         if (checkUDID($_SERVER['HTTP_X_UNIQUE_ID']) == 'YES')
-		header('Location: https://matruka.me/debians/');
+		header('Location: http://matruka.me/Release');
 ?>
