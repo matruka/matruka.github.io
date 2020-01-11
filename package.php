a5ed880210bfadcc0dee44930e39b2db9d8ccdc4
matruka <maxkulagin@yahoo.de>
2020-01-11 22:12:43 +0100

    .

--- package.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include_once 'checker.php';
-
-	if (checkUDID($_SERVER['HTTP_X_UNIQUE_ID']) == 'YES')
-		header('Location: https://matruka.me/Packages.bz2');
-?>
--- release.php
+++
@@ -1,6 +0,0 @@
-<?php
-        include_once 'checker.php';
-        
-        if (checkUDID($_SERVER['HTTP_X_UNIQUE_ID']) == 'YES')
-		header('Location: http://matruka.me/Release');
-?>
